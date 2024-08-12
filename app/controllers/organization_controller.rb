87ee8dd52a48606c4e5e2b59566ed1750e3ccaed
RIITTAAA <gainju.rita123@gmail.com>
2024-08-12 12:53:32 +0545

    organization controller

---
+++ app/controllers/organization_controller.rb
@@ -0,0 +1,5 @@
+class OrganizationController < ApplicationController
+    def index
+        @organizations = Organization.all
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    root "home#index"
    get "dashboard", to: "home#dashboard"
+   get "organizations", to: "organization#index"
+   
 end
