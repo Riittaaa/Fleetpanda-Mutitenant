e4aee5b774d69b32b9eaaa2b48173f5fb066fcbc
RIITTAAA <gainju.rita123@gmail.com>
2024-08-13 00:37:28 +0545

    Blogs as per the organizations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   has_many :memberships
   has_many :organizations, through: :memberships
+
+  attr_accessor :organization_id
+
+  after_create :create_membership
+
+  private
+
+  def create_membership
+    return unless organization_id.present?
+
+    memberships.create(organization_id: organization_id)
+  end
 end
