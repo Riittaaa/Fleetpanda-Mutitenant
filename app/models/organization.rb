90d076b7224960cd23b59fa7a181915deb016415
RIITTAAA <gainju.rita123@gmail.com>
2024-08-12 13:21:25 +0545

    Memberships mdel

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ApplicationRecord
-    validates :name, presence: true
+  validates :name, presence: true
+  has_many: memberships
+  has_many: users, through: :memberships
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :memberships
+  resources :models
   devise_for :users
 
   get "up" => "rails/health#show", as: :rails_health_check
